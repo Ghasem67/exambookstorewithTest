3ea53e599172065e953435928a2388c6e2b04aad
Ghasem67 <81895501+Ghasem67@users.noreply.github.com>
2022-04-24 23:41:45 +0430

    add migration

---
+++ src/BookStore.Migrations/20220424Init.cs
@@ -0,0 +1,44 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Migrations
+{
+    [Migration(20220424)]
+    public class _20220424Init : Migration
+    {
+        public override void Up()
+        {
+            CreateBooks();
+            CreateCategory();
+        }
+        public override void Down()
+        {
+            Delete.Table("Books");
+            Delete.Table("Categories");
+        }
+        private void CreateBooks()
+        {
+            Create.Table("Books")
+                            .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                            .WithColumn("Title").AsString(50).NotNullable()
+                            .WithColumn("Pages").AsInt32().NotNullable()
+                            .WithColumn("Description").AsString(500).Nullable()
+                            .WithColumn("Author").AsString(100).NotNullable()
+                            .WithColumn("CategoryId").AsInt32().NotNullable()
+                            .ForeignKey("FK_Books_Categories", "Categories", "Id")
+                            .OnDelete(System.Data.Rule.None);
+        }
+
+        private void CreateCategory()
+        {
+            Create.Table("Categories")
+                            .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                            .WithColumn("Title").AsString(50).NotNullable();
+        }
+
+    }
+}
---
+++ src/BookStore.Migrations/Program.cs
@@ -0,0 +1,70 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace BookStore.Migrations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = GetSettings(args, Directory.GetCurrentDirectory());
+
+            var connectionString = options.ConnectionString;
+
+            CreateDatabase(connectionString);
+
+            var runner = CreateRunner(connectionString, options);
+
+            runner.MigrateUp();
+        }
+
+        static void CreateDatabase(string connectionString)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            if (File.Exists(connectionStringBuilder.DataSource) == false)
+            {
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                connection.Close();
+            }
+        }
+
+        static IMigrationRunner CreateRunner(string connectionString, MigrationSettings options)
+        {
+            var container = new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSQLite()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(typeof(Program).Assembly).For.All())
+                .AddSingleton<MigrationSettings>(options)
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider();
+            return container.GetRequiredService<IMigrationRunner>();
+        }
+
+        private static MigrationSettings GetSettings(string[] args, string baseDir)
+        {
+            var configurations = new ConfigurationBuilder()
+                .SetBasePath(baseDir)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            var settings = new MigrationSettings();
+            configurations.Bind(settings);
+            return settings;
+        }
+    }
+
+    public class MigrationSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+
+}
